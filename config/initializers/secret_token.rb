47012ec050a25424f79ac88373dc1a2eed4489f0
pkim <pdkim85@gmail.com>
2012-11-13 21:13:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peterkimblog::Application.config.secret_token = 'f77a04c811123029301ea0f076aee8856abd85962b0d42edf0ee60e080c14398fbdcd04bb46b36ae7f8aedc7db376e972de841f203ca5d8a8ab562c46975ac0a'
