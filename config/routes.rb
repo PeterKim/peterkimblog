57889e2139ebd41564e4fe68a1262a6416370ae4
pkim <pdkim85@gmail.com>
2012-11-15 15:28:22 -0500

    static pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Peterkimblog::Application.routes.draw do
   get "pages/home"
-
   get "pages/help"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
