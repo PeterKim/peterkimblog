5865042332ae0c899f40cd625f436ba00a965076
pkim <pdkim85@gmail.com>
2012-11-15 19:56:43 -0500

    adding tests for links and helper

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,16 +1,1 @@
-def full_title(page_title)
-  base_title = "peterkimblog"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
-
-def get_heading(page_title)
-  if page_title != "Home"
-    "<div class=\"center\"><h1>#{page_title}</h1></div>"
-  else
-    ""
-  end
-end
+include ApplicationHelper
