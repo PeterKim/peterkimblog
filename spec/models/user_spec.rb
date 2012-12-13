512ed75c8dd6cb48e9952ff7a92933fa67c6a5f8
peter <pdkim85@gmail.com>
2012-12-14 04:07:26 +0900

    email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name:"example",email:"example@yahoo.com") }
+  before { @user = User.new(name:"example", email:"example@yahoo.com", password: "1234", password_confirmation: "1234") }
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
   
   it { should be_valid }
 
@@ -32,5 +47,34 @@ describe User do
         @user.should_not be_valid
       end
     end
+  end
+  
+  describe "when email address is already taken" do
+    before do 
+      dup_user = @user.dup
+      dup_user.email = @user.email.upcase
+      dup_user.save
+    end
+    it { should_not be_valid }
+  end
+  
+  describe "password should not be blank" do
+    before { @user.password = @user.password_confirmation = " "  }
+    it { should_not be_valid }
+  end
+  
+  describe "password should match" do 
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "password confirmation should not be nil" do
+    before { @user.password_confirmation = "nil" }
+    it { should_not be_valid }
+  end
+  
+  describe "password shouldn't be too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should_not be_valid }
   end
 end
