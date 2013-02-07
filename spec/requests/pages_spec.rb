ec5b49abd3e6e568686a95c62d9616b4cca17eb9
peter <pdkim85@gmail.com>
2013-02-07 08:36:02 -0500

    adding tests for sign up page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -11,7 +11,7 @@ describe "Pages" do
 
   describe "Home page" do
     before { visit root_path }
-    let(:heading) { "peterkimblog" }
+    let(:heading) { "Welcome!" }
     let(:page_title) { "" }
     it_should_behave_like "all pages"
     it { should_not have_selector('title', text: ' | Home') }
