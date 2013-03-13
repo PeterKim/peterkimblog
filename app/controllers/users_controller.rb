17f5d4c411b433abd0e0e33691b14835507dfd42
peter <pdkim85@gmail.com>
2013-03-13 08:20:07 -0400

    commenting user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update]
   before_filter :correct_user, only: [:edit, :update]
-
-  def new
-    @user = User.new
-    @submitBtnText = "Create my account"
+  
+  # users GET /users
+  def index
   end
   
-  def show
-    @user = User.find(params[:id])
-  end
-
+  # users POST /users
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -21,11 +17,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  # new_user GET /users/new
+  def new
+    @user = User.new
+    @submitBtnText = "Create my account"
+  end
   
+  # edit_user GET /user/id/edit
   def edit
     @submitBtnText = "Save changes"
   end
   
+  # user GET /user/id
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  # user PUT /user/id
   def update
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
@@ -36,6 +45,10 @@ class UsersController < ApplicationController
     end
   end
   
+  # user DELETE /user/id
+  def destroy
+  end
+
   private
   def signed_in_user
     unless signed_in?
