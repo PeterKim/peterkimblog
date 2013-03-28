b4186b6ad5c5b4dd644feec286806a7de71192eb
peter <pdkim85@gmail.com>
2013-03-28 08:38:34 -0400

    adding user/micropost relations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 require 'spec_helper'
@@ -28,6 +29,7 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
   
   it { should be_valid }
   it { should_not be_admin }
