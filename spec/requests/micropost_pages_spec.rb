4dc8b55a158e5f111edfe17303e8da521a73ced4
peter <pdkim85@gmail.com>
2013-04-09 07:53:31 -0400

    micropost interface & model completed

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -5,6 +5,7 @@ describe "MicropostPages" do
   let(:user) { FactoryGirl.create(:user) }
   before { sign_in user }
   
+  # create
   describe "micropost creation" do
     before { visit root_path }
     
@@ -25,5 +26,17 @@ describe "MicropostPages" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
       end
     end
+  end
+  
+  # destroy 
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+    
+    describe "as correct user" do
+      before { visit root_path }
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end 
   end
 end
